d6da1fd4496276b319bf625d686e7efbf774c76d
Jack Bowler <jcb2238@columbia.edu>
2016-09-12 09:23:37 -0400

    catch NumberFormatExceptions to allow for empy JSONArrays to be input

--- src/TrialAttrsForm.java
+++ src/TrialAttrsForm.java
@@ -36,228 +36,6 @@ import org.json.JSONException;
 import java.lang.NumberFormatException;
 
 
-interface JSONInputField {
-    public Object getValue();
-}
-
-class taf_LabeledInput extends JPanel implements JSONInputField {
-    private JLabel label;
-    private JTextField textField;
-
-    public taf_LabeledInput(String labelText) {
-        label = new JLabel(labelText);
-        textField = new JTextField("", 15);
-
-        label.setPreferredSize(
-            new Dimension(150,label.getPreferredSize().height));
-        add(label);
-        add(textField);
-    }
-
-    public String getValue() {
-        return textField.getText();
-    }
-}
-
-
-class taf_JSONArrayInput extends taf_JSONObjectInput implements JSONInputField {
-    private JButton addFieldButton;
-    private ActionListener al;
-   
-    private class JSONArrayInputActionListener implements ActionListener {
-        private taf_JSONArrayInput arrayInput;
-
-        public JSONArrayInputActionListener(taf_JSONArrayInput arrayInput) {
-            this.arrayInput = arrayInput;
-        }
-
-        public void actionPerformed(ActionEvent e) {
-            arrayInput.addArrayField();
-        }
-    }
-
-    public taf_JSONArrayInput(JSONArray json) throws JSONException {
-        super(json);
-        addFieldButton = null;
-        this.al = new JSONArrayInputActionListener(this);
-    }
-
-    public void setAddFieldButton(JButton button) {
-        if (addFieldButton != null) {
-            addFieldButton.removeActionListener(al);
-        }
-        button.addActionListener(al);
-        this.addFieldButton = button;
-    }
-
-    public void addArrayField() {
-        try {
-            addInput(fields.getJSONObject(0));
-        } catch (JSONException e) { }
-
-        revalidate();
-    }
-
-    public JSONArray getValue() {
-        JSONArray result = new JSONArray();
-        JSONObject field;
-        try {
-            field = fields.getJSONObject(0);
-        } catch (JSONException e) {
-            return fields;
-        }
-
-        for (int i=0; i<inputBoxes.size(); i++) {
-            try {
-                String type = field.getString("type");
-                if (type.equals("JSONObject") ||
-                        type.equals("JSONArray")) {
-                    result.put((JSONArray) inputBoxes.get(i).getValue());
-                } else {
-                    String value = (String) inputBoxes.get(i).getValue();
-                    if (value.equals("")) {
-                        continue;
-                    }
-
-                    if (type.equals("String")) {
-                        result.put(value);
-                    } else if (type.equals("int")) {
-                        try {
-                            result.put(Integer.parseInt(value));
-                        } catch (NumberFormatException e) { }
-                    } else if (type.equals("float")) {
-                        try {
-                            result.put(Double.parseDouble(value));
-                        } catch (NumberFormatException e) { }
-                    }
-                }
-            } catch (JSONException f) {
-                f.printStackTrace();
-            }
-        }
-
-        return result;
-    }
-
-}
-
-class taf_JSONObjectInput extends JPanel implements JSONInputField {
-    protected ArrayList<JSONInputField> inputBoxes;
-    protected JSONArray fields;
-    
-    public taf_JSONObjectInput(JSONArray json) throws JSONException {
-        inputBoxes = new ArrayList<JSONInputField>();
-        fields = json;
-
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        Border blackline = BorderFactory.createLineBorder(Color.black);
-        setBorder(blackline);
-
-        for (int i = 0; i < json.length(); i++) {
-            JSONObject field = json.getJSONObject(i);
-            addInput(field);
-        }
-    }
-
-    protected void addInput(JSONObject field) {
-        String key;
-        try {
-            key = field.getString("key");
-        } catch (JSONException e) {
-            return;
-        }
-
-        String label;
-        try {
-            label = field.getString("label");
-        } catch (JSONException e) {
-            label = key;
-        }
-
-        try {
-            if (field.getString("type").equals("JSONObject")) {
-                add(new JLabel("<html><b>" + label + "</b></html>"));
-                taf_JSONObjectInput jsonInput =
-                    new taf_JSONObjectInput(field.getJSONArray("fields"));
-                add(jsonInput);
-                inputBoxes.add(jsonInput);
-                return;
-            }  else if (field.getString("type").equals("JSONArray")) {
-                JPanel labelPanel = new JPanel();
-                labelPanel.add(new JLabel("<html><b>" + label + "</b></html>"));
-                JButton addField = new JButton("+");
-                labelPanel.add(addField);
-                add(labelPanel);
-                taf_JSONArrayInput jsonInput =
-                    new taf_JSONArrayInput(field.getJSONArray("fields"));
-                jsonInput.setAddFieldButton(addField);
-                add(jsonInput);
-                inputBoxes.add(jsonInput);
-                return;
-            }
-        } catch (JSONException e) { }
-
-        taf_LabeledInput input = new taf_LabeledInput(label);
-        add(input);
-        inputBoxes.add(input);
-    }
-
-    public int numFields() {
-        return inputBoxes.size();
-    }
-
-    public JSONArray getValue() {
-        for (int i=0; i<fields.length(); i++) {
-            try {
-                JSONObject field = fields.getJSONObject(i);
-                String type = field.getString("type");
-
-                if (type.equals("String")) {
-                    try {
-                        field.put("value", (String)inputBoxes.get(i).getValue());
-                    } catch (JSONException e) {
-                        try {
-                            field.put("value", "");
-                        } catch (JSONException f) {}
-                    }
-                } else if (type.equals("int")) {
-                    try {
-                        field.put("value",
-                            Integer.parseInt((String)inputBoxes.get(i).getValue()));
-                    } catch (JSONException e) {
-                        try {
-                            field.put("value", 0);
-                        } catch (JSONException f) {}
-                    }
-                } else if (type.equals("float")) {
-                    try {
-                        field.put("value",
-                            Double.parseDouble((String)inputBoxes.get(i).getValue()));
-                    } catch (JSONException e) {
-                        try {
-                            field.put("value", 0.0f);
-                        } catch (JSONException f) {}
-                    }
-                } else if (type.equals("JSONObject") ||
-                        type.equals("JSONArray")) {
-                    try {
-                        field.put("value",
-                            (JSONArray) inputBoxes.get(i).getValue());
-                    } catch (JSONException e) {
-                        try {
-                            field.put("value", new JSONArray());
-                        } catch (JSONException f) {}
-                    }
-                }
-            } catch (JSONException f) {
-                f.printStackTrace();
-            }
-        }
-
-        return fields;
-    }
-}
-
 public class TrialAttrsForm extends JDialog implements ActionListener {
     private JButton fileChooserButton;
     private JComboBox comboBox;
@@ -267,10 +45,237 @@ public class TrialAttrsForm extends JDialog implements ActionListener {
     private String selectedFile;
     private String selectedTag;
     private ActionListener actionListener;
-    private taf_JSONObjectInput fieldsInput;
+    private JSONObjectInput fieldsInput;
     private JPanel input_container;
 
     private ArrayList<JLabel> inputBoxes;
+
+    private interface JSONInputField {
+        public Object getValue();
+    }
+
+    private class LabeledInput extends JPanel implements JSONInputField {
+        private JLabel label;
+        private JTextField textField;
+
+        public LabeledInput(String labelText) {
+            label = new JLabel(labelText);
+            textField = new JTextField("", 15);
+
+            label.setPreferredSize(
+                new Dimension(150,label.getPreferredSize().height));
+            add(label);
+            add(textField);
+        }
+
+        public String getValue() {
+            return textField.getText();
+        }
+    }
+
+
+    class JSONArrayInput extends JSONObjectInput implements JSONInputField {
+        private JButton addFieldButton;
+        private ActionListener al;
+       
+        private class JSONArrayInputActionListener implements ActionListener {
+            private JSONArrayInput arrayInput;
+
+            public JSONArrayInputActionListener(JSONArrayInput arrayInput) {
+                this.arrayInput = arrayInput;
+            }
+
+            public void actionPerformed(ActionEvent e) {
+                arrayInput.addArrayField();
+            }
+        }
+
+        public JSONArrayInput(JSONArray json) throws JSONException {
+            super(json);
+            addFieldButton = null;
+            this.al = new JSONArrayInputActionListener(this);
+        }
+
+        public void setAddFieldButton(JButton button) {
+            if (addFieldButton != null) {
+                addFieldButton.removeActionListener(al);
+            }
+            button.addActionListener(al);
+            this.addFieldButton = button;
+        }
+
+        public void addArrayField() {
+            try {
+                addInput(fields.getJSONObject(0));
+            } catch (JSONException e) { }
+
+            revalidate();
+        }
+
+        public JSONArray getValue() {
+            JSONArray result = new JSONArray();
+            JSONObject field;
+            try {
+                field = fields.getJSONObject(0);
+            } catch (JSONException e) {
+                return fields;
+            }
+
+            for (int i=0; i<inputBoxes.size(); i++) {
+                try {
+                    String type = field.getString("type");
+                    if (type.equals("JSONObject") ||
+                            type.equals("JSONArray")) {
+                        try {
+                            result.put((JSONArray) inputBoxes.get(i).getValue());
+                        } catch (NumberFormatException e) { }
+                    } else {
+                        String value = (String) inputBoxes.get(i).getValue();
+                        if (value.equals("")) {
+                            continue;
+                        }
+
+                        if (type.equals("String")) {
+                            result.put(value);
+                        } else if (type.equals("int")) {
+                            try {
+                                result.put(Integer.parseInt(value));
+                            } catch (NumberFormatException e) { }
+                        } else if (type.equals("float")) {
+                            try {
+                                result.put(Double.parseDouble(value));
+                            } catch (NumberFormatException e) { }
+                        }
+                    }
+                } catch (JSONException f) {
+                    f.printStackTrace();
+                }
+            }
+
+            return result;
+        }
+
+    }
+
+    class JSONObjectInput extends JPanel implements JSONInputField {
+        protected ArrayList<JSONInputField> inputBoxes;
+        protected JSONArray fields;
+        
+        public JSONObjectInput(JSONArray json) throws JSONException {
+            inputBoxes = new ArrayList<JSONInputField>();
+            fields = json;
+
+            setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+            Border blackline = BorderFactory.createLineBorder(Color.black);
+            setBorder(blackline);
+
+            for (int i = 0; i < json.length(); i++) {
+                JSONObject field = json.getJSONObject(i);
+                addInput(field);
+            }
+        }
+
+        protected void addInput(JSONObject field) {
+            String key;
+            try {
+                key = field.getString("key");
+            } catch (JSONException e) {
+                return;
+            }
+
+            String label;
+            try {
+                label = field.getString("label");
+            } catch (JSONException e) {
+                label = key;
+            }
+
+            try {
+                if (field.getString("type").equals("JSONObject")) {
+                    add(new JLabel("<html><b>" + label + "</b></html>"));
+                    JSONObjectInput jsonInput =
+                        new JSONObjectInput(field.getJSONArray("fields"));
+                    add(jsonInput);
+                    inputBoxes.add(jsonInput);
+                    return;
+                }  else if (field.getString("type").equals("JSONArray")) {
+                    JPanel labelPanel = new JPanel();
+                    labelPanel.add(new JLabel("<html><b>" + label + "</b></html>"));
+                    JButton addField = new JButton("+");
+                    labelPanel.add(addField);
+                    add(labelPanel);
+                    JSONArrayInput jsonInput =
+                        new JSONArrayInput(field.getJSONArray("fields"));
+                    jsonInput.setAddFieldButton(addField);
+                    add(jsonInput);
+                    inputBoxes.add(jsonInput);
+                    return;
+                }
+            } catch (JSONException e) { }
+
+            LabeledInput input = new LabeledInput(label);
+            add(input);
+            inputBoxes.add(input);
+        }
+
+        public int numFields() {
+            return inputBoxes.size();
+        }
+
+        public JSONArray getValue() {
+            for (int i=0; i<fields.length(); i++) {
+                try {
+                    JSONObject field = fields.getJSONObject(i);
+                    String type = field.getString("type");
+
+                    if (type.equals("String")) {
+                        try {
+                            field.put("value", (String)inputBoxes.get(i).getValue());
+                        } catch (JSONException e) {
+                            try {
+                                field.put("value", "");
+                            } catch (JSONException f) {}
+                        }
+                    } else if (type.equals("int")) {
+                        try {
+                            field.put("value",
+                                Integer.parseInt((String)inputBoxes.get(i).getValue()));
+                        } catch (JSONException e) {
+                            try {
+                                field.put("value", 0);
+                            } catch (JSONException f) {}
+                        }
+                    } else if (type.equals("float")) {
+                        try {
+                            field.put("value",
+                                Double.parseDouble((String)inputBoxes.get(i).getValue()));
+                        } catch (JSONException e) {
+                            try {
+                                field.put("value", 0.0f);
+                            } catch (JSONException f) {}
+                        }
+                    } else if (type.equals("JSONObject") ||
+                            type.equals("JSONArray")) {
+                        try {
+                            field.put("value",
+                                (JSONArray) inputBoxes.get(i).getValue());
+                        } catch (JSONException e) {
+                            try {
+                                field.put("value", new JSONArray());
+                            } catch (JSONException f) {}
+                        }
+                    }
+                } catch (JSONException f) {
+                    f.printStackTrace();
+                }
+            }
+
+            return fields;
+        }
+    }
+
+
+
 
     public TrialAttrsForm(Component parent) {
         setTitle("Trial Attributes");
@@ -313,7 +318,7 @@ public class TrialAttrsForm extends JDialog implements ActionListener {
         } catch (JSONException e) {}
 
         try {
-            fieldsInput = new taf_JSONObjectInput(fields);
+            fieldsInput = new JSONObjectInput(fields);
             input_container.add(fieldsInput);
         } catch (JSONException e) {
             e.printStackTrace();
